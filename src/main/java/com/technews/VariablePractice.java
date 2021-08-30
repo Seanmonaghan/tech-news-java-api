107c0ce4e5b7bbdbc851f0dfda26b12265b2b872
Sean Monaghan <smonagha@conncoll.edu>
2021-08-30 16:45:09 -0400

    tested creating and combining variabels in Java

---
+++ src/main/java/com/technews/VariablePractice.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+public class VariablePractice {
+    public static void main (String[] args) {
+        int ten = 10;
+        double twenty = 20.00;
+        String c = "cat";
+        String concat = ten + c;
+        double sum = ten + twenty;
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+}
